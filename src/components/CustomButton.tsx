414b8835c34b2d8b27e405a1af2d7af4b56b6aa1
Martin <idonot@like.spam>
2025-02-03 10:47:16 -0500

    initial_commit

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet, ActivityIndicator } from "react-native";
+
+type CustomButtonProps = {
+  onPress?: () => Promise<void> | void;
+  title: string;
+  style: object;
+  textStyle?: object;
+  shadow?: object;
+  IconComponent?: React.ComponentType<any>;
+  EndIconComponent?: React.ComponentType<any>;
+  isDisabled?: boolean;
+  isLoading?: boolean;
+  loadingIndicatorColor?: string;
+};
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  onPress,
+  title,
+  style,
+  textStyle,
+  shadow,
+  IconComponent,
+  EndIconComponent,
+  isDisabled = false,
+  isLoading = false,
+  loadingIndicatorColor = "#fff",
+}) => (
+  <TouchableOpacity
+    style={[styles.button, style, shadow]}
+    onPress={onPress}
+    disabled={isDisabled || isLoading}
+  >
+    {isLoading ? (
+      <ActivityIndicator size="small" color={loadingIndicatorColor} />
+    ) : (
+      <>
+        {IconComponent && <IconComponent />}
+        {title && <Text style={[styles.buttonText, textStyle]}>{title}</Text>}
+        {EndIconComponent && <EndIconComponent />}
+      </>
+    )}
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    borderRadius: 100,
+    justifyContent: "center",
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
+
+export default CustomButton;
---
+++ src/utils/colors.ts
@@ -0,0 +1,24 @@
+export const colors = {
+	black: "#000000",
+	white: "#FFFFFF",
+	gray: "#939393",
+	lightGray: "#E8E6DC",
+	red: "#F74439",
+  
+	whiteSalty: "#F7F6F0",
+	whiteCreamy: "#F2F1DE",
+  
+	greenKale: "#0E4C45",
+	greenSalad: "#49AF29",
+	greenCauliflower: "#F7F3B6",
+	greenApple: "#D1DE57",
+  
+	pinkRaddish: "#FF92BE",
+	pinkRaspberry: "#FFCEDE",
+  
+	dragonFruit: "#EF5292",
+	orangeCarrot: "#FF7F00",
+	yellowPear: "#FFC533",
+	peachyBiege: "#FFEDC0",
+  };
+  
